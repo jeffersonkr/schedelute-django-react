1dcc0d4e3efcada446e777deebc9e4bd85253b43
jefferson kwak <jeffersonkr@hotmail.com>
2021-05-03 02:25:36 -0300

    fix folder namer

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Apply database migrations
 echo "Apply database migrations"
-python schedule_api/manage.py makemigrations core
+python schedule/manage.py makemigrations core
 
 # Apply database migrations
 echo "Apply database migrations"
-python schedule_api/manage.py migrate
+python schedule/manage.py migrate
 
 # Start server
 echo "Starting server"
-python schedule_api/manage.py runserver 172.25.0.10:8000
+python schedule/manage.py runserver 172.25.0.10:8000
