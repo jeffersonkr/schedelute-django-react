95cc1a3ef43f00ff527bf0de8ff2186ed545ce84
jefferson kwak <jeffersonkr@hotmail.com>
2021-05-02 21:04:17 -0300

    created models to schedelute and doctor

--- schedelute_api/core/models.py
+++ schedelute_api/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_mysql.models import JSONField
 
-# Create your models here.
+
+class Schedelute(models.Model):
+    date = models.DateField()
+    schedule_time = models.TimeField()
+    title = models.CharField(max_length=100)
+    doctor = models.ForeignKey(
+        "Doctor",
+        on_delete=models.CASCADE,
+    )
+
+
+class Doctor(models.Model):
+    class Expertise(models.TextChoices):
+        GENERAL = "GNL", _("Clínico Geral")
+        PEDIATRICIAN = "PDN", _("Pediatra")
+        CARDIOLOGIST = "CDT", _("Cardiologista")
+
+    name = models.CharField(max_length=50)
+    expertise = models.CharField(
+        max_length=3,
+        choices=Expertise.choices,
+        default=Expertise.GENERAL,
+    )
+    schedule_times = JSONField()
