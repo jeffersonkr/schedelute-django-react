dcba8d3cbdf07a32ff35bee487bbed7e962cd226
Jefferson Kwak <jeffersonkr@hotmail.com>
2021-05-03 05:35:04 -0300

    add npm into docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Apply database migrations
-echo "Apply database migrations"
+echo "making migrations"
 python schedule/manage.py makemigrations core
 
 # Apply database migrations
 echo "Apply database migrations"
 python schedule/manage.py migrate
 
+# Starting npm server
+echo "Running NPM"
+cd schedule/schedule_frontend
+npm run dev &
+cd /code
+
 # Start server
 echo "Starting server"
-python schedule/manage.py runserver 172.25.0.10:8000
+python schedule/manage.py runserver 172.25.0.10:8000 
+
